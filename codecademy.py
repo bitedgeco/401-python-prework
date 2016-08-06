16bfffb646284081b9d679442aaeec9f7ae578a3
James <canning.jimmy@gmail.com>
2016-08-06 16:42:45 -0700

    add code from codecademy

---
+++ codecademy.py
@@ -0,0 +1,188 @@
+############################################################
+# Unit 3: PygLatin - part 10
+############################################################
+pyg = 'ay'
+
+original = raw_input('Enter a word:')
+
+if len(original) > 0 and original.isalpha():
+    word = original.lower()
+    first = word[0]
+    
+    new_word = word + first + pyg
+    new_word = new_word[1:]
+    
+    print new_word
+else:
+    print 'empty'
+
+############################################################
+# Unit 4: Taking a Vacation - part 7
+############################################################
+def hotel_cost(nights):
+    return 140 * nights
+    
+def plane_ride_cost(city):
+    if city == "Charlotte":
+        return 183
+    elif city == "Tampa":
+        return 220
+    elif city == "Pittsburgh":
+        return 222
+    elif city == "Los Angeles":
+        return 475
+        
+def rental_car_cost(days):
+    cost = 40 * days
+    if days >= 7:
+        cost -= 50
+    elif days >= 3:
+        cost -= 20
+    return cost
+
+def trip_cost(city, days, spending_money):
+    return rental_car_cost(days) + hotel_cost(days) + plane_ride_cost(city) + spending_money
+    
+print trip_cost("Los Angeles", 5, 600)
+
+############################################################
+# Unit 5: A Day at the Supermarket - part 12
+############################################################
+shopping_list = ["banana", "orange", "apple"]
+
+stock = {
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15
+}
+    
+prices = {
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
+
+# Write your code below!
+def compute_bill(food):
+    total = 0
+    for key in food:
+        if stock[key] != 0:
+            total = total + prices[key]
+            stock[key] = stock[key] -1
+    return total
+
+############################################################
+# Unit 6: Student Becomes the Teacher - part 9
+############################################################
+lloyd = {
+    "name": "Lloyd",
+    "homework": [90.0, 97.0, 75.0, 92.0],
+    "quizzes": [88.0, 40.0, 94.0],
+    "tests": [75.0, 90.0]
+}
+alice = {
+    "name": "Alice",
+    "homework": [100.0, 92.0, 98.0, 100.0],
+    "quizzes": [82.0, 83.0, 91.0],
+    "tests": [89.0, 97.0]
+}
+tyler = {
+    "name": "Tyler",
+    "homework": [0.0, 87.0, 75.0, 22.0],
+    "quizzes": [0.0, 75.0, 78.0],
+    "tests": [100.0, 100.0]
+}
+
+# Add your function below!
+def average(numbers):
+    total = sum(numbers)
+    total = float(total)
+    average = total / len(numbers)
+    return average
+
+def get_average(student):
+    homework = average(student["homework"])
+    quizzes = average(student["quizzes"])
+    tests = average(student["tests"])
+    sum = homework * .1 + quizzes * .3 + tests * .6
+    return sum
+
+def get_letter_grade(score):
+    if score >= 90:
+        return "A"
+    elif score >= 80:
+        return "B"
+    elif score >= 70:
+        return "C"
+    elif score >= 60:
+        return "D" 
+    else:
+        return "F"
+    
+print get_letter_grade(get_average(student))
+    
+def get_class_average(students):
+    results = []
+    for student in students:
+        av = get_average(student)
+        results.append(av)
+    return average(results)
+
+print get_class_average(students)
+
+print get_letter_grade(get_class_average(students))
+
+############################################################
+# Unit 7: Battleship! - part 18
+############################################################
+from random import randint
+
+board = []
+
+for x in range(5):
+    board.append(["O"] * 5)
+
+def print_board(board):
+    for row in board:
+        print " ".join(row)
+
+print "Let's play Battleship!"
+print_board(board)
+
+def random_row(board):
+    return randint(0, len(board) - 1)
+
+def random_col(board):
+    return randint(0, len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+print ship_row
+print ship_col
+
+# Everything from here on should go in your for loop!
+# Be sure to indent four spaces!
+
+for turn in range(4):
+
+    guess_row = int(raw_input("Guess Row:"))
+    guess_col = int(raw_input("Guess Col:"))
+
+    if guess_row == ship_row and guess_col == ship_col:
+        print "Congratulations! You sunk my battleship!"
+        break
+    else:
+        if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+            print "Oops, that's not even in the ocean."
+        elif(board[guess_row][guess_col] == "X"):
+            print "You guessed that one already."
+        else:
+            print "You missed my battleship!"
+            board[guess_row][guess_col] = "X"
+            print 'turn ='
+            print turn + 1
+            print_board(board)
+    if turn == 3:
+        print "Game Over"
